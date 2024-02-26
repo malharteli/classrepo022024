0d9d7b7be5f1e4220add14d64f2132d6054c2c40
Malhar Teli <malhar.teli@gmail.com>
2024-02-26 12:06:41 -0600

    Answer for BracketChecker

--- classwork/src/strings/BracketChecker.java
+++ classwork/src/strings/BracketChecker.java
@@ -1,18 +1,25 @@
 package strings;
 
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 // given a string of braces
 // return true if braces are terminated accurately with the correct end brace
+//Example Input: "()"
+//Example out: true
+//Example Input: "({})"
+//Example Output: true
+//Example Input: "([()})"
+//Example: false
+//Example: "([)]"
+//Output: false
+//Example: "([]{})"
+//Output: true
 public class BracketChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String inputString = sc.nextLine();
         boolean returnBool = checkerReplaceAll(inputString);
-        boolean returnBool2 = checkerDeque(inputString);
+        boolean returnBool2 = checkerStack(inputString);
         System.out.println(returnBool);
         System.out.println(returnBool2);
     }
@@ -20,15 +27,15 @@ public class BracketChecker {
     public static boolean checkerReplaceAll(String input){
         String workString = input;
         while(workString.contains("()") || workString.contains("[]")|| workString.contains("{}")){
-            workString = workString.replaceAll("\\(\\)","")
-                    .replaceAll("\\{}","")
-                    .replaceAll("\\[]","");
+            workString = workString.replaceAll("\\(\\)","") // ()
+                    .replaceAll("\\{}","") //{}
+                    .replaceAll("\\[]",""); //[]
         }
         return (workString.length()==0);
     }
 
     public static  boolean checkerDeque(String input){
-        Deque<Character> deque = new LinkedList<>();
+        Deque<Character> deque = new LinkedList<>(); //Last in First out
         for (Character ch : input.toCharArray()){
             if (ch =='{'|| ch=='[' || ch == '('){
                 deque.addFirst(ch);
@@ -42,6 +49,23 @@ public class BracketChecker {
             }
         }
         return deque.size()==0;
+    }
+
+    public static boolean checkerStack(String input){
+        Stack<Character> stack = new Stack<>();
+        for (Character ch: input.toCharArray()){
+            if (ch=='('|| ch=='{'|| ch=='['){
+                stack.push(ch);
+            }
+            else if (!stack.isEmpty()){
+                if (stack.peek() == '(' && ch ==')' || stack.peek()=='[' && ch ==']' || stack.peek() == '{' && ch == '}'){
+                    stack.pop();
+                }
+                else
+                    return false;
+            }
+        }
+        return stack.isEmpty();
     }
 
 
